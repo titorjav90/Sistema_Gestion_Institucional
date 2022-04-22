7c89621515609433adabafd2d9a6e8a909261cf8
Hector Montecino <104215566+titorjav90@users.noreply.github.com>
2022-04-22 12:28:59 -0300

    Add files via upload

---
+++ class_lib/prece/home.php
@@ -0,0 +1,16 @@
+			<div id="panel" class="left"><!-- panel -->
+			<div id="lateral"><!-- lateral -->
+				<p>
+					&#160;&#160;&#160;&#160;<img src="img/icon_rector.png"/> &#160;&#160;&#160;&#160;<b>Preceptor</b><br/><br/>
+					[ <a href="#">Carga de Nuevo Alumno</a> ]<br/>
+					[ <a href="#">Limpieza de contraseñas</a> ]<br/>
+					[ <a href="#">Cursos (listado - asistencias)</a> ]<br/>
+					[ <a href="#">Solicitud de constancias</a> ]<br/>
+				</p>
+			</div> <!-- lateral -->
+
+			<div id="app">	<!-- #app -->
+			APP - APP - APP - APP - APP - APP
+			</div>			<!-- #app -->
+
+		</div><!-- -- | panel | -- -->
